43cb828dff08f9b74ff19026365221a158656a26
henri <couvreurhenri@gmail.com>
2021-03-11 23:48:08 +0100

    11/3

---
+++ Assets/Scripts/Units/UnitRangedHeal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UnitRangedHeal : MonoBehaviour {
+    [Header("Balancing")]
+    public float healAmount;
+    public float attackAnimDuration;
+    
+    [Header("References")]
+    public UnitRanged unitRanged;
+
+    public Unit unit => unitRanged.unit;
+
+    public void Awake() {
+        unitRanged.OnAttack.AddListener(Heal);
+    }
+
+    public void Heal() {
+        Game.m.PlaySound(MedievalCombat.MAGIC_BUFF_ATTACK, .5f, 2);
+        this.Wait(attackAnimDuration, ApplyBuff);
+    }
+
+    public void ApplyBuff() {
+        if (unit.status != Unit.Status.ALIVE) return;
+
+        Unit healTarget = unit.allies.Except(unit).Random();
+        if (healTarget == null) return;
+        
+        healTarget.AddHealth(healAmount);
+    }
+}
